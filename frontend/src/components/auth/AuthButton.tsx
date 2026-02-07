df123d8084723eb35d1269efeaa080288e03b3a6
new-CarlosEst <c.estebandiez@gmail.com>
2026-02-07 18:33:23 +0100

    Fix: Backend container removed
    He borrado el contendor del backend y para desarrollo lo correre en local, ya que con el contendor no funciona el hotreload, asi que no se actualiza la pagina

--- frontend/src/components/auth/AuthButton.tsx
+++ frontend/src/components/auth/AuthButton.tsx
@@ -1,6 +1,6 @@
 function AuthButton(){
     return (
-        <button className="bg-gray-200 h-8 w-20 border-black border-2 text-center rounded-2xl hover: ">
+        <button className="bg-gray-200 h-8 w-20 border-black border-2 text-center rounded-2xl hover:bg-blue-300">
             Login
         </button>
     )
