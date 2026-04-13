5d997df6bc0bf3a8b0d0d2ba49b7ccdc9b81e609
new-CarlosEst <c.estebandiez@gmail.com>
2026-04-13 17:39:30 +0200

    Fix: quitados los imports que no uso en frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from './pages/Landing';
 import Header from "./components/layouts/Header/Header";
-import AuthModal from "./components/auth/AuthModal";
 
-//TODO: Hacer header, boton login/registro, como el modal con los forms cuando vayan ya los endpoints
 function App() {
   return (
     <>
