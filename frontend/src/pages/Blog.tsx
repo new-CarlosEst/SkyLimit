f5cd6f4922b5c56c4ba3e15d282f63416474fb07
new-CarlosEst <c.estebandiez@gmail.com>
2026-04-17 11:34:09 +0200

    Hacer que se vea el h1

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,7 +1,7 @@
 function Blog() {
     return (
         <div>
-            <h1>Esta sera la pagina del Blog de skylimit</h1>
+            <h1 className="text-black mt-16">Esta sera la pagina del Blog de skylimit</h1>
         </div>
     );
 }
