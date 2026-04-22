d46e2fa1a42496cf05533bf3e1ce65bfab5eabd5
new-CarlosEst <c.estebandiez@gmail.com>
2026-04-22 18:59:06 +0200

    Arreglados errores de importanciones

--- frontend/src/store/flightSearchStore.ts
+++ frontend/src/store/flightSearchStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import type { FlightResult, SearchResults } from "../types/flight.types"; //Pone que FlightResult no se usa, pero lo necesito para el tipo de dato
+import type { SearchResults } from "../types/flight.types";
 
 //Store para guardar los resultados de búsqueda de vuelos
 export const useFlightSearchStore = create<SearchResults>()(
