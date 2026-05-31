48bab84fe54a0bb7604ed60b3705d7bf956864e0
new-CarlosEst <c.estebandiez@gmail.com>
2026-05-31 23:04:23 +0200

    Metodos con vuelos reales

--- backend/src/apiflight/apiflight.controller.ts
+++ backend/src/apiflight/apiflight.controller.ts
@@ -22,9 +22,8 @@ export class ApiflightController {
     @Query('children') children?: string,
     @Query('infants') infants?: string,
   ) {
-    // TODO: CAMBIAR A LA API REAL ANTES DE SUBIR A PRODUCCION
-    const apiFlights = await this.apiflightService.getFlightsRoundTripMock();
-    //const apiFlights = await this.apiflightService.getFlightsRoundTrip(originName, destinationName, origin, departure, cabinClass, date, returnDate, adults, children, infants);
+    //const apiFlights = await this.apiflightService.getFlightsRoundTripMock();
+    const apiFlights = await this.apiflightService.getFlightsRoundTrip(originName, destinationName, origin, departure, cabinClass, date, returnDate, adults, children, infants);
 
     // Obtener vuelos de la base de datos interna
     const dbFlights = await this.flightService.searchRoundTripFlights({
@@ -54,9 +53,8 @@ export class ApiflightController {
     @Query('children') children?: string,
     @Query('infants') infants?: string,
   ) {
-    // TODO: CAMBIAR A LA API REAL ANTES DE SUBIR A PRODUCCION
-    const apiFlights = await this.apiflightService.getFlightsOneWayMock();
-    //const apiFlights = await this.apiflightService.getFlightsOneWay(originName, destinationName, origin, departure, cabinClass, date, adults, children, infants);
+    //const apiFlights = await this.apiflightService.getFlightsOneWayMock();
+    const apiFlights = await this.apiflightService.getFlightsOneWay(originName, destinationName, origin, departure, cabinClass, date, adults, children, infants);
 
     // Obtener vuelos de la base de datos interna
     const dbFlights = await this.flightService.searchOneWayFlights({
@@ -80,9 +78,8 @@ export class ApiflightController {
   ) {
     const legsArray = JSON.parse(legs);
 
-    // TODO: CAMBIAR A LA API REAL ANTES DE SUBIR A PRODUCCION
-    const apiFlights = await this.apiflightService.getFlightsMultiMock();
-    //const apiFlights = await this.apiflightService.getFlightsMulti(legsArray, cabinClass, adults);
+    //const apiFlights = await this.apiflightService.getFlightsMultiMock();
+    const apiFlights = await this.apiflightService.getFlightsMulti(legsArray, cabinClass, adults);
 
     // Obtener vuelos de la base de datos interna
     const dbFlights = await this.flightService.searchMultiFlights({
